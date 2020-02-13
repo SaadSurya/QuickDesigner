702443b5b5bd4cd55e7fc5eb57b1ae7d01d380e8
Saad Surya <saadsurya@hotmail.com>
2020-02-13 08:59:12 +0500

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.ImagesBrowse = new System.Windows.Forms.Button();
+            this.ZipsFolderPathTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CDRFilesLocationTxt = new System.Windows.Forms.TextBox();
+            this.CDRBrowse = new System.Windows.Forms.Button();
+            this.GenerateBtn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.errorConsoleTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ImagesBrowse
+            // 
+            this.ImagesBrowse.Location = new System.Drawing.Point(669, 40);
+            this.ImagesBrowse.Name = "ImagesBrowse";
+            this.ImagesBrowse.Size = new System.Drawing.Size(75, 23);
+            this.ImagesBrowse.TabIndex = 0;
+            this.ImagesBrowse.Text = "Browse";
+            this.ImagesBrowse.UseVisualStyleBackColor = true;
+            this.ImagesBrowse.Click += new System.EventHandler(this.ImagesBrowse_Click);
+            // 
+            // ZipsFolderPathTxt
+            // 
+            this.ZipsFolderPathTxt.Location = new System.Drawing.Point(51, 40);
+            this.ZipsFolderPathTxt.Name = "ZipsFolderPathTxt";
+            this.ZipsFolderPathTxt.Size = new System.Drawing.Size(612, 20);
+            this.ZipsFolderPathTxt.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Zip Files Source Folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "\"CDR\" Files Destination Folder";
+            // 
+            // CDRFilesLocationTxt
+            // 
+            this.CDRFilesLocationTxt.Location = new System.Drawing.Point(51, 99);
+            this.CDRFilesLocationTxt.Name = "CDRFilesLocationTxt";
+            this.CDRFilesLocationTxt.Size = new System.Drawing.Size(612, 20);
+            this.CDRFilesLocationTxt.TabIndex = 4;
+            // 
+            // CDRBrowse
+            // 
+            this.CDRBrowse.Location = new System.Drawing.Point(669, 97);
+            this.CDRBrowse.Name = "CDRBrowse";
+            this.CDRBrowse.Size = new System.Drawing.Size(75, 23);
+            this.CDRBrowse.TabIndex = 3;
+            this.CDRBrowse.Text = "Browse";
+            this.CDRBrowse.UseVisualStyleBackColor = true;
+            this.CDRBrowse.Click += new System.EventHandler(this.CDRBrowse_Click);
+            // 
+            // GenerateBtn
+            // 
+            this.GenerateBtn.Location = new System.Drawing.Point(360, 149);
+            this.GenerateBtn.Name = "GenerateBtn";
+            this.GenerateBtn.Size = new System.Drawing.Size(75, 23);
+            this.GenerateBtn.TabIndex = 6;
+            this.GenerateBtn.Text = "Generate";
+            this.GenerateBtn.UseVisualStyleBackColor = true;
+            this.GenerateBtn.Click += new System.EventHandler(this.GenerateBtn_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(51, 194);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(693, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 7;
+            // 
+            // errorConsoleTxt
+            // 
+            this.errorConsoleTxt.Location = new System.Drawing.Point(51, 243);
+            this.errorConsoleTxt.Multiline = true;
+            this.errorConsoleTxt.Name = "errorConsoleTxt";
+            this.errorConsoleTxt.ReadOnly = true;
+            this.errorConsoleTxt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.errorConsoleTxt.Size = new System.Drawing.Size(693, 154);
+            this.errorConsoleTxt.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Error Console";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 422);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.errorConsoleTxt);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.GenerateBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CDRFilesLocationTxt);
+            this.Controls.Add(this.CDRBrowse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ZipsFolderPathTxt);
+            this.Controls.Add(this.ImagesBrowse);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Quick Desinger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button ImagesBrowse;
+        private System.Windows.Forms.TextBox ZipsFolderPathTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox CDRFilesLocationTxt;
+        private System.Windows.Forms.Button CDRBrowse;
+        private System.Windows.Forms.Button GenerateBtn;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox errorConsoleTxt;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,374 @@
+﻿using Svg;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private const double PageWidth = 210;
+        private const double PageHeight = 297;
+        private const double MaxWidth = 80;
+        private const double MaxHeight = 95;
+        private const double MaxTextWidth = 70;
+
+        private const double MarginLeft = 5;
+        private const double MarginTop = (PageHeight - (MaxHeight * 3)) / 2;
+        private const double MarginRight = 40;
+        private const double MarginBottom = MarginTop;
+
+        private string extractPath = "";
+        private string processedZipFilesPath = "";
+
+        private int counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            ZipsFolderPathTxt.Text = @"C:\Project\Resources\Zips";
+            CDRFilesLocationTxt.Text = @"C:\Project\Resources\Templates";
+
+        }
+
+
+
+        private async void GenerateBtn_Click(object sender, EventArgs e)
+        {
+            transformImageFile(@"D:\Projects\CorelDRAWAddon\Resources\Sample\New bactch of files\026-6084095-0164338\0e921bbb-d250-14b9-27a3-b85e631688b5.svg");
+
+            //if (CDRFilesLocationTxt.Text == "")
+            //{
+            //    MessageBox.Show("Please select CDR files location");
+            //    return;
+            //}
+            //if (ZipsFolderPathTxt.Text == "")
+            //{
+            //    MessageBox.Show("Please select zip files location");
+            //    return;
+            //}
+            //try
+            //{
+
+            //    disableEveryThing();
+            //    var progress = new Progress<object>(v => {
+            //        if (v is int)
+            //        {
+            //            progressBar1.Value = (int)v;
+            //        }
+            //        else
+            //        {
+            //            errorConsoleTxt.Text += v.ToString();
+            //        }
+            //    });
+            //    processedZipFilesPath = ZipsFolderPathTxt.Text + @"\processed\";
+            //    extractPath = CDRFilesLocationTxt.Text + @"\temp\";
+            //    await Task.Run(() => generate(progress));
+            //    MessageBox.Show("Files generated successfully!");
+            //    enableEveryThing();
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //    enableEveryThing();
+            //}
+        }
+
+        private void ImagesBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog(this);
+            if (result == DialogResult.OK)
+            {
+                ZipsFolderPathTxt.Text = folderBrowserDialog1.SelectedPath;
+            }
+
+
+
+        }
+
+        private void CDRBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog(this);
+            if (result == DialogResult.OK)
+            {
+                CDRFilesLocationTxt.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private string[] getZipFiles(string directoryPath)
+        {
+            Directory.CreateDirectory(processedZipFilesPath);
+            DirectoryInfo directory = new DirectoryInfo(directoryPath);
+            return directory.GetFiles("*.zip").OrderBy(p => p.CreationTime).Select(f => f.FullName).ToArray();
+        }
+        private List<string> getImagesFromZip(string zipFilePath)
+        {
+            Directory.CreateDirectory(extractPath);
+            emptyTempDirectory();
+            List<string> imageFiles = new List<string>();
+            using (ZipArchive archive = ZipFile.OpenRead(zipFilePath))
+            {
+                int count = 0;
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    if (entry.FullName.EndsWith(".svg", StringComparison.OrdinalIgnoreCase)
+                            || entry.FullName.EndsWith(".png", StringComparison.OrdinalIgnoreCase)
+                            || entry.FullName.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase)
+                            || entry.FullName.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase)
+                            )
+                    {
+                        // Gets the full path to ensure that relative segments are removed.
+                        string destinationPath = Path.GetFullPath(Path.Combine(extractPath, entry.FullName));
+
+                        // Ordinal match is safest, case-sensitive volumes can be mounted within volumes that
+                        // are case-insensitive.
+                        if (destinationPath.StartsWith(extractPath, StringComparison.Ordinal))
+                        {
+                            entry.ExtractToFile(destinationPath);
+                            if (Path.GetExtension(destinationPath).EndsWith(".svg", StringComparison.OrdinalIgnoreCase))
+                            {
+                                imageFiles.Add(destinationPath);
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+            return imageFiles;
+        }
+
+        private VGCore.Shape loadResizedImage(VGCore.Layer layer, string imageFilePath)
+        {
+            double adjustedMaxWidth = getMaxWidth(imageFilePath);
+            transformImageFile(imageFilePath);
+            layer.ImportEx(imageFilePath).Finish();
+            VGCore.Shape image = layer.FindShape(Path.GetFileName(imageFilePath));
+            if (image.SizeWidth > adjustedMaxWidth || image.SizeHeight > MaxHeight)
+            {
+                if (image.SizeWidth >= image.SizeHeight)
+                {
+                    double diff = (image.SizeWidth - adjustedMaxWidth) / image.SizeWidth;
+                    image.SizeWidth = adjustedMaxWidth;
+                    image.SizeHeight = (1 - diff) * image.SizeHeight;
+
+                }
+                else
+                {
+                    double diff = (image.SizeHeight - MaxHeight) / image.SizeHeight;
+                    image.SizeHeight = MaxHeight;
+                    image.SizeWidth = (1 - diff) * image.SizeWidth;
+                }
+            }
+            if (image.SizeWidth > adjustedMaxWidth)
+            {
+                double diff = (image.SizeWidth - adjustedMaxWidth) / image.SizeWidth;
+                image.SizeWidth = adjustedMaxWidth;
+                image.SizeHeight = (1 - diff) * image.SizeHeight;
+            }
+            if (image.SizeHeight > MaxHeight)
+            {
+                double diff = (image.SizeHeight - MaxHeight) / image.SizeHeight;
+                image.SizeHeight = MaxHeight;
+                image.SizeWidth = (1 - diff) * image.SizeWidth;
+            }
+            return image;
+        }
+
+        private void transformImageFile(string imageFilePath)
+        {
+            SvgDocument document = SvgDocument.Open(imageFilePath);
+            for (int i = 0; i < document.Children.Count; i++) {
+                SvgElement child = document.Children[i];
+                if (child.GetType().Name == "SvgGroup" && child.ContainsAttribute("clip-path") && child.Children.Count > 0) {
+                    SvgElement innerGroup = document.Children[i] = child.Children[0];
+                    List<SvgElement> toBeRemoved = innerGroup.Children.Where(e => !new string[] { "SvgImage", "SvgText" }.Contains(e.GetType().Name)).ToList();
+                    foreach(var e in toBeRemoved) {
+                        innerGroup.Children.Remove(e);
+                    }
+                }
+            }
+            File.WriteAllText(imageFilePath, document.GetXML());
+        }
+
+        private double getMaxWidth(string imageFilePath)
+        {
+            double maxWidth = MaxWidth;
+            SvgDocument document = SvgDocument.Open(imageFilePath);
+            if (document.GetXML().Contains("<text") && !document.GetXML().Contains("<image"))
+            {
+                return MaxTextWidth;
+            }
+            return maxWidth;
+        }
+
+        private void emptyTempDirectory()
+        {
+            string tempDirectoryPath = CDRFilesLocationTxt.Text + @"\temp\";
+            DirectoryInfo di = new DirectoryInfo(tempDirectoryPath);
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+        }
+
+        private VGCore.Document newDocument(VGCore.Application application)
+        {
+            VGCore.Document document = application.CreateDocument();
+            document.Name = ++counter + "_" + Guid.NewGuid().ToString();
+            document.Activate();
+            document.Unit = VGCore.cdrUnit.cdrMillimeter;
+            document.ActivePage.SetSize(210, 297);
+            document.Rulers.VUnits = document.Rulers.HUnits = VGCore.cdrUnit.cdrMillimeter;
+            createGuideLines(document);
+            return document;
+        }
+
+        private void saveCloseDocument(VGCore.Document document)
+        {
+            document.SaveAs(CDRFilesLocationTxt.Text + "\\" + document.Name + ".cdr");
+            document.Close();
+        }
+        private void disableEveryThing()
+        {
+            ZipsFolderPathTxt.Enabled =
+                ImagesBrowse.Enabled =
+                CDRFilesLocationTxt.Enabled =
+                CDRBrowse.Enabled =
+                GenerateBtn.Enabled =
+                false;
+            errorConsoleTxt.Text = "";
+            counter = 0;
+        }
+        private void enableEveryThing()
+        {
+            ZipsFolderPathTxt.Enabled =
+                ImagesBrowse.Enabled =
+                CDRFilesLocationTxt.Enabled =
+                CDRBrowse.Enabled =
+                GenerateBtn.Enabled =
+                true;
+            progressBar1.Value = 0;
+        }
+
+        private void generate(IProgress<object> progress)
+        {
+            VGCore.Application application;
+            try
+            {
+                application = new VGCore.Application();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("CorelDRAW is not started or its version is not compatible with the software, please start CorelDRAW if not started or install compatible version");
+            }
+            VGCore.Document document = null;
+            double positionX = 0;
+            double positionY = 0;
+            List<string> processedZipFiles = new List<string>();
+            string[] zipFiles = getZipFiles(ZipsFolderPathTxt.Text);
+            int zipCount = 1;
+            foreach (string zipFilePath in zipFiles)
+            {
+                if ((zipCount - 1) % 3 == 0)
+                {
+                    if (document != null)
+                    {
+                        saveCloseDocument(document);
+                        moveZipFiles(processedZipFiles);
+                    }
+                    document = newDocument(application);
+                    positionX = MarginLeft;
+                    //positionY = PageHeight + MaxHeight;
+                    positionY = PageHeight - MarginTop + MaxHeight;
+                }
+                VGCore.Layer layer = document.ActiveLayer;
+                List<string> imageFiles = getImagesFromZip(zipFilePath);
+
+                bool isFirstImage = true;
+                foreach (string imageFilePath in imageFiles)
+                {
+                    VGCore.Shape image;
+                    try
+                    {
+                        image = loadResizedImage(layer, imageFilePath);
+                    }
+                    catch (Exception ex)
+                    {
+                        string message = "Failed to load file '" + Path.GetFileName(imageFilePath) + "' from zip file '" + Path.GetFileName(zipFilePath);
+                        progress.Report(message + "' for template file '" + document.Name + ".cdr'" + Environment.NewLine);
+                        //image = layer.CreateArtisticText(0, 0, message);
+                        //image.SizeWidth = MaxWidth;
+                        //image.SizeHeight = MaxHeight;
+                        //image.WrapText = VGCore.cdrWrapStyle.cdrWrapSquareAboveBelow;
+                        continue;
+
+                    }
+                    double x;
+                    double y;
+                    if (isFirstImage)
+                    {
+                        positionX = MarginLeft;
+                        //positionY -= MaxHeight + MarginTop;
+                        positionY -= MaxHeight;
+                        x = positionX + ((MaxWidth - image.SizeWidth) / 2);
+                        y = positionY - (MaxHeight - image.SizeHeight) / 2;
+                        isFirstImage = false;
+                    }
+                    else
+                    {
+                        //positionX = PageWidth - MarginRight - image.SizeWidth;
+                        positionX += MaxWidth + MarginRight;
+                        x = positionX + ((MaxWidth - image.SizeWidth) / 2);
+                        y = positionY - (MaxHeight - image.SizeHeight) / 2;
+                    }
+
+                    image.SetPosition(x, y);
+                }
+                progress.Report(((100 - 10) / zipFiles.Length) * zipCount);
+                zipCount++;
+                processedZipFiles.Add(zipFilePath);
+            }
+            saveCloseDocument(document);
+            moveZipFiles(processedZipFiles);
+            application.Refresh();
+            emptyTempDirectory();
+            progress.Report(100);
+        }
+        private void moveZipFiles(List<string> zipFiles)
+        {
+            foreach (string zipFile in zipFiles)
+            {
+                File.Move(zipFile, processedZipFilesPath + Path.GetFileName(zipFile));
+            }
+            zipFiles.RemoveAll(s => true);
+        }
+        private void createGuideLines(VGCore.Document document)
+        {
+            //Horizontal Guides
+            double y = PageHeight - MarginTop;
+            document.ActiveLayer.CreateGuide(0, y, PageWidth, y);
+            for (int i = 0; i < 7; i++)
+            {
+                y -= MaxHeight / 2;
+                document.ActiveLayer.CreateGuide(0, y, PageWidth, y);
+            }
+            //Vertical Guides
+            double x = MarginLeft;
+            for (int i = 0; i < 6; i++)
+            {
+                document.ActiveLayer.CreateGuide(x, 0, x, PageHeight);
+                x += MaxWidth;
+                document.ActiveLayer.CreateGuide(x, 0, x, PageHeight);
+                x += MarginRight;
+            }
+        }
+
+    }
+}
